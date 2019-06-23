fde88439afb28b6ec7b53d3c1b75772f12e651e9
Tran Van Tai <tooyoutooyou@gmail.com>
2019-06-23 18:26:41 +0700

    angular8-routingComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { TrangChuComponent } from './home/trang-chu/trang-chu.component';
+import { TinTucComponent } from './home/tin-tuc/tin-tuc.component';
+import { DanhSachPhimComponent } from './home/danh-sach-phim/danh-sach-phim.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Routing Home Page
+  {
+    path: "", component: HomeComponent, children: [
+      { path: "trang-chu", component: TrangChuComponent },
+      { path: "tin-tuc", component: TinTucComponent },
+      { path: "danh-sach-phim", component: DanhSachPhimComponent }
+  ]},
+  // { path: "home", component: HomeComponent },
+  {path: "admin",component: AdminComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+import { TrangChuComponent } from './home/trang-chu/trang-chu.component';
+import { TinTucComponent } from './home/tin-tuc/tin-tuc.component';
+import { DanhSachPhimComponent } from './home/danh-sach-phim/danh-sach-phim.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    HomeComponent,
+    TrangChuComponent,
+    TinTucComponent,
+    DanhSachPhimComponent
   ],
   imports: [
     BrowserModule,
